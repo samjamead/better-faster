8824a5e073b3ee47f6b5b2f87a6194bfd79cc58c
Sam Mead <samjamead@gmail.com>
2025-02-21 22:59:04 +0000

    Tweak footer

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,24 +1,45 @@
 import Image from "next/image";
 import githubMark from "@/public/github-mark.svg";
 import githubMarkWhite from "@/public/github-mark-white.svg";
-import { cn } from "@/lib/utils";
-export default function Footer() {
+import { format } from "date-fns";
+
+export default async function Footer() {
+  const data = await fetch(
+    "https://api.github.com/repos/samjamead/better-faster/commits/main",
+  );
+  const repoData = await data.json();
   return (
     <footer className="border-t px-3 md:px-8">
-      <div className={cn("mx-auto flex justify-end py-8")}>
-        <p className="flex items-center gap-2 rounded border px-3 py-2 font-mono text-sm shadow">
+      <div className="flex flex-col items-center justify-between py-8 md:flex-row">
+        <div className="flex flex-col gap-4 text-sm">
+          <p>Made with ❤️ in the Channel Islands</p>
+          <p>
+            Last updated by commit{" "}
+            <a
+              href={`https://github.com/samjamead/better-faster/commits/main/`}
+              target="_blank"
+              className="rounded bg-secondary/30 px-1 py-0.5 font-semibold"
+            >
+              {repoData.sha.slice(0, 7)}
+            </a>{" "}
+            on{" "}
+            {format(new Date(repoData.commit.author.date), "EEEE d MMMM, yyyy")}
+          </p>
+        </div>
+
+        <p className="flex items-center gap-2 rounded border px-4 py-2 font-mono text-sm shadow">
           <Image
             src={githubMark}
             alt="GitHub"
-            width={16}
-            height={16}
+            width={14}
+            height={14}
             className="dark:hidden"
           />
           <Image
             src={githubMarkWhite}
             alt="GitHub"
-            width={16}
-            height={16}
+            width={14}
+            height={14}
             className="hidden dark:block"
           />
           <a
