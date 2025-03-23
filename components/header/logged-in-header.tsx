8409e98e009bf3bc6ec20dd0e363186b8c7588f0
Sam Mead <samjamead@gmail.com>
2025-03-23 12:40:54 +0000

    Improve header messages

--- components/header/logged-in-header.tsx
+++ components/header/logged-in-header.tsx
@@ -6,18 +6,39 @@ import Breadcrumbs from "./breadcrumbs";
 import { getGolferSummary } from "./server-actions";
 
 type GolferSummaryProps = {
-  data: { name: string; handicap: number; holes_per_birdie: number } | null;
+  data: {
+    name: string;
+    handicap: number;
+    holes_per_birdie: number;
+    most_played_course: string;
+    holes_played: number;
+  } | null;
 };
 
 export const GolferSummary = ({ data }: GolferSummaryProps) => {
   if (!data) return null;
 
-  return (
-    <p>
-      {data.name} ({data.handicap}) makes birdie every {data.holes_per_birdie}{" "}
-      holes
-    </p>
-  );
+  // Check if all values except name are null/0
+  const isNewGolfer =
+    !data.holes_per_birdie && !data.most_played_course && !data.holes_played;
+
+  if (isNewGolfer) {
+    return <p>Hi {data.name}! Welcome to Better Faster</p>;
+  }
+
+  const messages = [
+    ...(data.holes_per_birdie > 0
+      ? [
+          `${data.name} (${data.handicap}) makes birdie every ${data.holes_per_birdie} holes`,
+        ]
+      : []),
+    `${data.name} (${data.handicap}) most played course is ${data.most_played_course}`,
+    `${data.name} (${data.handicap}) has played ${data.holes_played} holes`,
+  ];
+
+  const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+
+  return <p>{randomMessage}</p>;
 };
 
 export default function LoggedInHeader() {
