978c07f1755a35f12ba25d63a0270a80d09ed4a5
Sam Mead <samjamead@gmail.com>
2025-10-12 09:08:41 +0100

    Whoops!

--- lib/calculate-course-handicap.ts
+++ lib/calculate-course-handicap.ts
@@ -1,8 +1,13 @@
-export const calculateCourseHandicap = (
-  handicapIndex: number,
-  courseRating: number,
-  slopeRating: number,
-  par: number,
-) => {
-  return handicapIndex * (113 / slopeRating) + (courseRating - par);
+export const calculateCourseHandicap = ({
+  handicapIndex,
+  courseRating = 72,
+  slopeRating = 113,
+  par = 72,
+}: {
+  handicapIndex: number;
+  courseRating: number;
+  slopeRating: number;
+  par: number;
+}) => {
+  return Math.round(handicapIndex * (slopeRating / 113) + (courseRating - par));
 };
